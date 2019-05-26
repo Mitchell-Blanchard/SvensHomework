0f58af22b184278ade1004d5f6ae4930d53409d6
Mitchell Blanchard <mitchell.94.b@gmail.com>
2019-05-26 18:05:18 +1000

    first draft

--- Powershell/Task 4 Network system Information/NSI.ps1
+++ Powershell/Task 4 Network system Information/NSI.ps1
@@ -1,1 +1,32 @@
+#Get-NetIPAddress -IPAddress 
+#Test-WSMan -ComputerName 192.168.43.23
+#$?
 
+$min = 0
+$max = 255
+
+$FirstOct =  (10,172,192)
+
+Test-Connection -count 1 -ComputerName
+
+
+for each $ip in $IPAddresses
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Internal Networks
+# 10.0.0.0 - 10.255.255.255
+# 172.16.0.0 - 172.31.255.255
+# 192.168.0.0 - 192.168.255.255
