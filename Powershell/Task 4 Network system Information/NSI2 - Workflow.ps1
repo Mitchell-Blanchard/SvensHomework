172dad07d832a4503b572227d40a49b3fec2f7d3
Mitchell Blanchard <mitchell.94.b@gmail.com>
2019-05-29 14:36:56 +1000

    Change how the scan worked, from Background jobs to parallel workflows

---
+++ Powershell/Task 4 Network system Information/NSI2 - Workflow.ps1
@@ -0,0 +1,27 @@
+﻿workflow Test-WinRM {
+    param($NetworkComputers)
+    foreach -Parallel -ThrottleLimit 28 ($Computer in $NetworkComputers.IPAddress) {
+
+            if (Test-WSMan -ErrorAction SilentlyContinue -ComputerName $Computer) {
+                echo "$Computer"
+        }
+    }
+}
+
+$WinRMEnabled = Test-WinRM -NetworkComputers $(Get-NetNeighbor -AddressFamily IPv4)
+
+$SelectedComputers =$WinRMEnabled | Out-GridView -PassThru -Title "IP Addresses"
+
+
+
+
+
+
+
+#RAM
+$RAM = Write-Host "$(((Get-CimInstance -Class Win32_PhysicalMemory -ComputerName .).Capacity | measure-object -Sum).Sum/1GB)GB"
+
+#HDD - Turn Size into GB
+(Get-CimInstance -Class Win32_DiskDrive -ComputerName .) | select Caption, Size | Where -property Size -NE $null
+
+#CPU - Get Name
