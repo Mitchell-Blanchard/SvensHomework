6fc3e30117dade4c749761c65bb6e9a5aa5128b4
Mitchell Blanchard <mitchell.94.b@gmail.com>
2019-05-30 11:50:20 +1000

    into gathering info from computers

--- Powershell/Task 4 Network system Information/NSI2 - Workflow.ps1
+++ Powershell/Task 4 Network system Information/NSI2 - Workflow.ps1
@@ -8,20 +8,26 @@
     }
 }
 
-$WinRMEnabled = Test-WinRM -NetworkComputers $(Get-NetNeighbor -AddressFamily IPv4)
+$WinRMEnabledIPs = Test-WinRM -NetworkComputers $(Get-NetNeighbor -AddressFamily IPv4)
 
-$SelectedComputers =$WinRMEnabled | Out-GridView -PassThru -Title "IP Addresses"
+#Create hashtable for 
 
+foreach ($IP in $WinRMEnabledIPs) {
+    $ComputerName = (Resolve-DnsName $IP).NameHost
+    $ComputerRAM = Write-Host "$(((Get-CimInstance -Class Win32_PhysicalMemory -ComputerName .).Capacity | measure-object -Sum).Sum/1GB)GB"
+    $ComputerHDD = (Get-CimInstance -Class Win32_DiskDrive -ComputerName .) | select Caption, Size | Where -property Size -NE $null
 
-
-
-
-
+    Set-Variable -Name "$HashTable($ComputerName)" -Value @{}
+    $(get-variable -Name "$HashTable($ComputerName)").add("ComputerName", "$ComputerName")
+    #$HashTable`_$ComputerName.Add("IPAddress", "$IP")
+    #$HashTable`_$ComputerName.Add("RAM", "$ComputerRAM")
+    #Get-Variable HashTable_${ComputerName}.Add("HDD", "$ComputerHDD")
+}
 
 #RAM
-$RAM = Write-Host "$(((Get-CimInstance -Class Win32_PhysicalMemory -ComputerName .).Capacity | measure-object -Sum).Sum/1GB)GB"
+#$RAM = Write-Host "$(((Get-CimInstance -Class Win32_PhysicalMemory -ComputerName .).Capacity | measure-object -Sum).Sum/1GB)GB"
 
 #HDD - Turn Size into GB
-(Get-CimInstance -Class Win32_DiskDrive -ComputerName .) | select Caption, Size | Where -property Size -NE $null
+#(Get-CimInstance -Class Win32_DiskDrive -ComputerName .) | select Caption, Size | Where -property Size -NE $null
 
 #CPU - Get Name
