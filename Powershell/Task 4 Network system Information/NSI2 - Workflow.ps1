337633fca0afcbe923afc1de28963211cd30c5f8
Mitchell Blanchard <mitchell.94.b@gmail.com>
2019-05-30 19:14:10 +1000

    added invoke command logic

--- Powershell/Task 4 Network system Information/NSI2 - Workflow.ps1
+++ Powershell/Task 4 Network system Information/NSI2 - Workflow.ps1
@@ -1,4 +1,6 @@
-﻿workflow Test-WinRM {
+﻿$ComputerInfo = @()
+
+workflow Test-WinRM {
     param($NetworkComputers)
     foreach -Parallel -ThrottleLimit 28 ($Computer in $NetworkComputers.IPAddress) {
 
@@ -10,24 +12,25 @@
 
 $WinRMEnabledIPs = Test-WinRM -NetworkComputers $(Get-NetNeighbor -AddressFamily IPv4)
 
-#Create hashtable for 
-
 foreach ($IP in $WinRMEnabledIPs) {
     $ComputerName = (Resolve-DnsName $IP).NameHost
-    $ComputerRAM = Write-Host "$(((Get-CimInstance -Class Win32_PhysicalMemory -ComputerName .).Capacity | measure-object -Sum).Sum/1GB)GB"
-    $ComputerHDD = (Get-CimInstance -Class Win32_DiskDrive -ComputerName .) | select Caption, Size | Where -property Size -NE $null
 
-    Set-Variable -Name "$HashTable($ComputerName)" -Value @{}
-    $(get-variable -Name "$HashTable($ComputerName)").add("ComputerName", "$ComputerName")
-    #$HashTable`_$ComputerName.Add("IPAddress", "$IP")
-    #$HashTable`_$ComputerName.Add("RAM", "$ComputerRAM")
-    #Get-Variable HashTable_${ComputerName}.Add("HDD", "$ComputerHDD")
+    $ComputerInfo += New-Object -TypeName PSObject -Property @{
+        "ComputerName" = "$ComputerName"
+        "IPAddress" = "$IP"
+    }
 }
 
-#RAM
-#$RAM = Write-Host "$(((Get-CimInstance -Class Win32_PhysicalMemory -ComputerName .).Capacity | measure-object -Sum).Sum/1GB)GB"
+$SelectedComputers = $ComputerInfo | Out-GridView -PassThru
 
-#HDD - Turn Size into GB
-#(Get-CimInstance -Class Win32_DiskDrive -ComputerName .) | select Caption, Size | Where -property Size -NE $null
+foreach ($computer in $SelectedComputers) {
+    $Hostname = $Computer.ComputerName
+    #$session = New-PSSession -ComputerName "gameplay10" -Credential administrator
+    Invoke-Command -Credential "$Hostname" -computerName $hostname -ScriptBlock {Get-Host}
+}
 
-#CPU - Get Name
+
+
+
+#make session and use that one session to get all the info needed
+#this means only 1 connection per computer needs to be made
