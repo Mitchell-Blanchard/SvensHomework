4929c43c7baebcf50dfcb25ed1b6c53d1a8b3dfd
Mitchell Blanchard <mitchell.94.b@gmail.com>
2019-05-31 17:09:13 +1000

    added function "computerconnections"

--- Powershell/Task 4 Network system Information/NSI2 - Workflow.ps1
+++ Powershell/Task 4 Network system Information/NSI2 - Workflow.ps1
@@ -18,19 +18,30 @@ foreach ($IP in $WinRMEnabledIPs) {
     $ComputerInfo += New-Object -TypeName PSObject -Property @{
         "ComputerName" = "$ComputerName"
         "IPAddress" = "$IP"
+        "RAM" = ""
+        "CPU" = ""
+        "HDDs" = ""
     }
 }
 
-$SelectedComputers = $ComputerInfo | Out-GridView -PassThru
+$SelectedComputers = ($ComputerInfo.ComputerName) | Out-GridView -PassThru
 
-foreach ($computer in $SelectedComputers) {
-    $Hostname = $Computer.ComputerName
-    #$session = New-PSSession -ComputerName "gameplay10" -Credential administrator
-    Invoke-Command -Credential "$Hostname" -computerName $hostname -ScriptBlock {Get-Host}
+function ComputerConnections {
+
+    foreach ($computer in $SelectedComputers) {
+        set-variable -name "session$computer" -value (New-PSSession -ComputerName $computer -Credential administrator) -Scope script
+    }
+
+    foreach ($computer in $SelectedComputers) {
+        $RAM = Invoke-Command -Session (get-variable -Name "session$computer").value -ScriptBlock {"$(((Get-CimInstance -Class Win32_PhysicalMemory -ComputerName $ComputerName).Capacity | measure-object -Sum).Sum/1GB)GB"}
+        $CPU = Invoke-Command -Session (get-variable -Name "session$computer").value -ScriptBlock {wmic cpu get name}
+        
+        $Drives = Invoke-Command -Session (get-variable -Name "session$computer").value -ScriptBlock {Get-CimInstance -Class Win32_DiskDrive | Where -property Size -NE $null}
+        $Drives = $Drives.Size | ForEach-Object {$_ /1GB}
+
+        $ComputerInfo | Where "ComputerName" -eq $computer | %{$_.RAM=$RAM}
+        $ComputerInfo | Where "ComputerName" -eq $computer | %{$_.CPU=$CPU.Name}
+        $ComputerInfo | Where "ComputerName" -eq $computer | %{$_.HDDs=$Drives}
+    }
+
 }
-
-
-
-
-#make session and use that one session to get all the info needed
-#this means only 1 connection per computer needs to be made
